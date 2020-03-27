ad692abff4f13f6d3311ba82468f7c012e7d9434
Henrique V. Ehrenfried <hvehrenfried@inf.ufpr.br>
2020-03-27 11:14:17 -0300

    Add locales.clj
    Signed-off-by: Henrique V. Ehrenfried <hvehrenfried@inf.ufpr.br>

--- resources/locales.clj
+++ resources/locales.clj
@@ -1,5 +1,5 @@
 {
-  :locales  #{"en" "zh" "zh-HK" "zh-TW" "uk" "fr" "nl" "tr" "sk" "it" "es" "nb" "ca" "de" "fa" "ja" "vi" "pl" "pt" "ru" }
+  :locales  #{"en" "zh-TW" "fa" "zh-HK" "pt" "de" "fr" "es" "ja" "pl" "it" "zh" "ru" "sk" "ca" "nl" }
   :packages ["metabase"]
   :bundle   "metabase.Messages"
 }
